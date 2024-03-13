a8534cec14b40e12db11e57d1dc069f646c5ec39
Marco Azzurrini <marcoazzurrini@Marcos-MacBook-Pro.local>
2024-03-13 12:55:37 +0100

    update content

--- src/data/presentation.ts
+++ src/data/presentation.ts
@@ -5,30 +5,34 @@ type Social = {
 
 type Presentation = {
   mail: string;
-  title: string;
-  description: string;
+  title: string[];
+  description: string[];
   socials: Social[];
   profile?: string;
 };
 
 const presentation: Presentation = {
-  mail: "maxencewolff.pro@gmail.com",
-  title: "Hi, I’m Maxence 👋",
+  mail: "guido.forloop@gmail.com",
+  title: ["Ciao! Sono Guido ✌️", "amichevole Recruiter di quartiere."],
   // profile: "/profile.webp",
-  description:
-    "Bonjour, i'm a *french frontend developer* with over *3 years* of web experience. I am currently working with *NextJS and Typescript*. Outside of work I complete my pokemon card collection and learning TypeScript.",
+  description: [
+    "I love getting into relationship with people, technology, dogs (but also cats), LinkedIn, tech communities, I even love developers. However, I don't know if the feeling is mutual.",
+    "My favorite word is 'abigeato.'",
+    "Lately I also love GitHub, if you want to browse you can find my repos here: https://github.com/GuidoPenta",
+    "Oh, I founded forloop.tech , the first company that offers Recruiting as a Service with subcriptions.",
+  ],
   socials: [
     {
       label: "X",
-      link: "https://twitter.com/itsstormzz_",
+      link: "https://twitter.com/guidopenta",
     },
     {
-      label: "Bento",
-      link: "https://bento.me/m-wolff",
+      label: "LinkedIn",
+      link: "https://www.linkedin.com/in/guido-penta/",
     },
     {
       label: "Github",
-      link: "https://github.com/MaeWolff",
+      link: "https://github.com/GuidoPenta",
     },
   ],
 };
--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,26 +1,27 @@
 export type Project = {
   title: string;
-  techs: string[];
+  description: string;
   link: string;
   isComingSoon?: boolean;
 };
 
 const projects: Project[] = [
   {
-    title: "Dictionary App",
-    techs: ["ReactJS (NextJS)", "react-query", "zod"],
-    link: "https://github.com/MaeWolff/dictionary-app",
+    title: "Guida Galattica per il CV",
+    description: "Il CV per il mercato italiano fatto come si deve! :)",
+    link: "https://github.com/GuidoPenta/galactic-CV-guide",
   },
   {
-    title: "Portfolio / Lina BLIDI",
-    techs: ["ReactJS (NextJS)", "TypeScript"],
-    link: "https://www.linablidi.fr/",
+    title: "Guida Galattica al profilo LinkedIn",
+    description:
+      "L'obiettivo è quello di darti degli spunti utili per rendere il tuo profilo LinkedIn attrattivo ma soprattutto 'keyword-friendly'.",
+    link: "https://github.com/GuidoPenta/galactic-linkedin-profile-guide",
   },
   {
-    title: "Portfolio / Template",
-    techs: ["Astro"],
-    link: "/",
-    isComingSoon: true,
+    title: "Italian tech communities",
+    description:
+      "Un insieme di communities, podcast, canali Twitch/Youtube e molto altro del tech italiano. Enjoy!",
+    link: "https://github.com/GuidoPenta/awesome-italian-tech-communities",
   },
 ];
 
